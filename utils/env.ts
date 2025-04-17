79c7f2b97c5127c544fa314b64502502a7bbb072
Nestor Otondo <notondo@byupathway.edu>
2025-04-17 11:36:31 -0300

    Refactorize environments variables

--- utils/env.ts
+++ utils/env.ts
@@ -1,13 +1,3 @@
-import dotenv from 'dotenv';
-
-dotenv.config();
-
-const { LOGIN_URL } = process.env;
-
-if (!LOGIN_URL) {
-  throw new Error('LOGIN_URL is not defined. Please set it in your environment variables.');
-}
-
 export const env = {
-  LOGIN_URL,
+  LOGIN_URL: process.env.LOGIN_URL ?? '',
 };
